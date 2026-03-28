2090e37bef2712b328225b8870bc3ced4035114c
Rahul Jain <rahulkj@gmail.com>
2026-03-28 12:12:33 -0500

    Fixng the git workflow

--- cmd/hardware.go
+++ cmd/hardware.go
@@ -116,22 +116,6 @@ func detectLinuxAvailableMemory() uint64 {
 	return 512 * 1024 * 1024
 }
 
-func detectDarwinMemory() uint64 {
-	val, err := syscall.Sysctl("hw.memsize")
-	if err != nil {
-		log.Printf("Warning: could not get hw.memsize: %v, using conservative default\n", err)
-		return 512 * 1024 * 1024
-	}
-
-	// sysctl returns little-endian bytes; right-pad to 8 bytes
-	b := make([]byte, 8)
-	copy(b, []byte(val))
-	physMem := uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
-		uint64(b[4])<<32 | uint64(b[5])<<40 | uint64(b[6])<<48 | uint64(b[7])<<56
-
-	return physMem * 60 / 100
-}
-
 func detectWindowsMemory() uint64 {
 	// Fallback for Windows - use conservative default
 	return 512 * 1024 * 1024
